939566811c30e8d23283add8b5dc99b7b7460bcd
Evdokimov27 <mr.evdokimov27@yandex.ru>
2024-04-11 13:10:09 +0800

    Update Code

--- BalletSchoolBot/Program.cs
+++ BalletSchoolBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Text;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
@@ -7,30 +8,69 @@ using Google.Apis.Sheets.v4.Data;
 using Newtonsoft.Json;
 class Program
 {
-    static string idInstance = "1103912324";
-    static string apiTokenInstance = "6e579cfae2194032b03f9c9d20fd7e6263ec7402b1d64f3384";
+    static string idInstance = "1103925412";
+    static string apiTokenInstance = "c2b3e8e316614008b83ae7492c843237828034e0f3ae420299";
     static readonly HttpClient client = new HttpClient();
-    static List<string> nomberReCall = new List<string> { "79642181512" };
-
+    static List<string> nomberReCall = new List<string> { "79041386602" };
+    static bool auth = false;
+    static Timer timer;
+	static TimeSpan startTime = new TimeSpan(18, 0, 0); // 18:00
+	static TimeSpan endTime = new TimeSpan(9, 0, 0); // 09:00
 	static async Task Main()
     {
         GetCreditorNomber();
-
-		while (true)
+        if (!auth)
         {
-            Console.WriteLine("Checking for messages and debts...");
-            await GetNotification();
-           // await SendDebtorMounth();
-           // await SendCreditor();
+            string urlAuth = $"https://api.green-api.com/waInstance{idInstance}/getStateInstance/{apiTokenInstance}";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(urlAuth);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Auth notifications = JsonConvert.DeserializeObject<Auth>(responseBody);
+                    if (responseBody != null && notifications?.stateInstance != "authorized")
+                    {
+                        TimeSpan interval = TimeSpan.FromHours(1);
+
+                        timer = new Timer(async _ =>
+                        {
+                            await SendCallNotification($"{nomberReCall}@c.us", $"Автоответчик не авторизован на green-api.com");
+                            auth = false;
+                        },
+                        null, TimeSpan.Zero, interval); // Запускаем сразу и повторяем каждый час
+
+                    }
+                    if (responseBody != null && notifications?.stateInstance == "authorized")
+                    {
+                        auth = true;
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
             await Task.Delay(1000); // Задержка в 1 секунду между запросами
         }
-    }
 
+        while (true)
+        {
+            await GetNotification();
+            // await SendDebtorMounth();
+            // await SendCreditor();
+            await Task.Delay(1000); // Задержка в 1 секунду между запросами
+        }
+
+
+    }
     static async Task SendCreditor()
     {
         List<string> idChats = new List<string> { "120363239727653355@c.us" };
         try
-        { 
+        {
             foreach (var chatId in idChats)
             {
                 // отправка сообщения раз в 3 дня
@@ -69,48 +109,45 @@ class Program
 
     static async Task GetNotification()
     {
-		DateTime currentTimeUtc = DateTime.UtcNow;
-
-		// Выбор часового пояса, например "Eastern Standard Time" для восточного побережья США
-		TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("North Asia East Standard Time");
-
-		// Конвертация UTC времени в выбранный часовой пояс
-		DateTime currentTimeZone = TimeZoneInfo.ConvertTimeFromUtc(currentTimeUtc, easternZone);
-		TimeSpan currentTime = currentTimeZone.TimeOfDay;
-		var startTime = new TimeSpan(10, 0, 0); // 18:00
-        var endTime = new TimeSpan(9, 0, 0); // 09:00
-
-		// Проверяем, находится ли текущее время вне диапазона с 09:00 до 18:00
-		if (currentTime > endTime && currentTime < startTime)
+        if (auth)
         {
-            Console.WriteLine($"Рабочее время {currentTime.Hours} ");
-            string urlTime = $"https://api.green-api.com/waInstance{idInstance}/receiveNotification/{apiTokenInstance}";
+            Console.WriteLine("Checking for messages and debts...");
+            DateTime currentTimeUtc = DateTime.UtcNow;
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("North Asia East Standard Time");
+            DateTime currentTimeZone = TimeZoneInfo.ConvertTimeFromUtc(currentTimeUtc, easternZone);
+            TimeSpan currentTime = currentTimeZone.TimeOfDay;
+            
 
-            try
+            if (currentTime > endTime && currentTime < startTime)
             {
-                HttpResponseMessage response = await client.GetAsync(urlTime);
-                if (response.IsSuccessStatusCode)
+                Console.WriteLine($"Рабочее время {currentTime.Hours} ");
+                string urlTime = $"https://api.green-api.com/waInstance{idInstance}/receiveNotification/{apiTokenInstance}";
+
+                try
                 {
-                    string responseBody = await response.Content.ReadAsStringAsync();
-
-                    Root notifications = JsonConvert.DeserializeObject<Root>(responseBody);
-
-
-                    if (responseBody != null && notifications?.ReceiptId != null)
+                    HttpResponseMessage response = await client.GetAsync(urlTime);
+                    if (response.IsSuccessStatusCode)
                     {
-                        await DeleteNotification(notifications.ReceiptId.ToString());
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        Root notifications = JsonConvert.DeserializeObject<Root>(responseBody);
+
+
+                        if (responseBody != null && notifications?.ReceiptId != null)
+                        {
+                            await DeleteNotification(notifications.ReceiptId.ToString());
+                        }
                     }
                 }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+                return;
             }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-            }
-            return;
-        }
-        Console.WriteLine("Не рабочее время.");
-        string url = $"https://api.green-api.com/waInstance{idInstance}/receiveNotification/{apiTokenInstance}";
+            Console.WriteLine("Не рабочее время.");
+            string url = $"https://api.green-api.com/waInstance{idInstance}/receiveNotification/{apiTokenInstance}";
 
             try
             {
@@ -124,14 +161,12 @@ class Program
 
                     if (responseBody != null && notifications?.ReceiptId != null)
                     {
-                        Console.WriteLine($"ReceiptId: {notifications.ReceiptId}");
-                        Console.WriteLine($"Type: {notifications.Body.TypeWebhook}");
-                        if (notifications.Body.TypeWebhook == "incomingCall" && notifications.Body.Status == "offer")
+                        if (notifications.Body.TypeWebhook == "incomingCall" && (notifications.Body.Status == "missed" || notifications.Body.Status == "declined"))
                         {
-                        foreach (var nomber in nomberReCall)
-                        {
-                            await SendCallNotification($"{nomber}@c.us", $"Вам звонил номер {notifications.Body.From.Substring(0, notifications.Body.From.Length - 5)} в нерабочее время!");
-                        }
+                            foreach (var nomber in nomberReCall)
+                            {
+                                await SendCallNotification($"{nomber}@c.us", $"Вам звонил номер {notifications.Body.From.Substring(0, notifications.Body.From.Length - 5)} в нерабочее время!");
+                            }
                             await SendCallNotification(notifications.Body.From, $"Вы позвонили в нерабочее время, мы уведомлены, что вы звонили и обязательно перезвоним вам!");
                         }
                         await DeleteNotification(notifications.ReceiptId.ToString());
@@ -143,98 +178,99 @@ class Program
                 Console.WriteLine("\nException Caught!");
                 Console.WriteLine("Message :{0} ", e.Message);
             }
+        }
     }
     static void GetCreditorNomber()
     {
-		string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-		string ApplicationName = "Google Sheets API .NET Quickstart";
-		string spreadsheetId = "1dpg2LYy3QA8YllteHdnQOYOnhLilUV2K0Q-LP8XDPAk";
-		string range = "Лист2!A:Z"; // Укажите диапазон, который хотите прочитать
+        string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        string ApplicationName = "Google Sheets API .NET Quickstart";
+        string spreadsheetId = "1dpg2LYy3QA8YllteHdnQOYOnhLilUV2K0Q-LP8XDPAk";
+        string range = "Лист2!A:Z"; // Укажите диапазон, который хотите прочитать
 
-		GoogleCredential credential;
+        GoogleCredential credential;
 
-		using (var stream = new FileStream("D://notificationcreditor-4f4dd8981059.json", FileMode.Open, FileAccess.Read))
-		{
-			credential = GoogleCredential.FromStream(stream)
-				.CreateScoped(Scopes);
-		}
+        using (var stream = new FileStream("notificationcreditor-c14f26f59510.json", FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream)
+                .CreateScoped(Scopes);
+        }
 
-		// Создание Google Sheets API сервиса.
-		var service = new SheetsService(new BaseClientService.Initializer()
-		{
-			HttpClientInitializer = credential,
-			ApplicationName = ApplicationName,
-		});
+        // Создание Google Sheets API сервиса.
+        var service = new SheetsService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
 
-		// Чтение данных
-		SpreadsheetsResource.ValuesResource.GetRequest request =
-				service.Spreadsheets.Values.Get(spreadsheetId, range);
+        // Чтение данных
+        SpreadsheetsResource.ValuesResource.GetRequest request =
+                service.Spreadsheets.Values.Get(spreadsheetId, range);
 
-		ValueRange response = request.Execute();
-		IList<IList<Object>> values = response.Values;
-		DateTime now = DateTime.Now;
+        ValueRange response = request.Execute();
+        IList<IList<Object>> values = response.Values;
+        DateTime now = DateTime.Now;
         int nowMountRow = 0;
 
-		switch (now.Month)
+        switch (now.Month)
         {
             case 9:
                 {
-					nowMountRow = 2; break;
+                    nowMountRow = 2; break;
                 }
             case 10:
                 {
-					nowMountRow = 3; break;
+                    nowMountRow = 3; break;
                 }
-			case 11:
-				{
-					nowMountRow = 4; break;
-				}
-			case 12:
-				{
-					nowMountRow = 5; break;
-				}
-			case 1:
-				{
-					nowMountRow = 6; break;
-				}
-			case 2:
-				{
-					nowMountRow = 7; break;
-				}
-			case 3:
-				{
-					nowMountRow = 8; break;
-				}
-			case 4:
-				{
-					nowMountRow = 9; break;
-				}
-			case 5:
-				{
-					nowMountRow = 10; break;
-				}
-			case 6:
-				{
-					nowMountRow = 11; break;
-				}
-		}
-		string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(now.Month);
+            case 11:
+                {
+                    nowMountRow = 4; break;
+                }
+            case 12:
+                {
+                    nowMountRow = 5; break;
+                }
+            case 1:
+                {
+                    nowMountRow = 6; break;
+                }
+            case 2:
+                {
+                    nowMountRow = 7; break;
+                }
+            case 3:
+                {
+                    nowMountRow = 8; break;
+                }
+            case 4:
+                {
+                    nowMountRow = 9; break;
+                }
+            case 5:
+                {
+                    nowMountRow = 10; break;
+                }
+            case 6:
+                {
+                    nowMountRow = 11; break;
+                }
+        }
+        string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(now.Month);
 
-		if (values != null && values.Count > 0)
-		{
+        if (values != null && values.Count > 0)
+        {
             foreach (var row in values)
             {
-				if ((long.TryParse(row[nowMountRow-1].ToString(), out long resultBackMounth) && resultBackMounth != 0) && (long.TryParse(row[nowMountRow].ToString(), out long resultCurMounth) && resultCurMounth == 0) && (long.TryParse(row[nowMountRow+1].ToString(), out long resultNextMounth) && resultNextMounth == 0))
-				{
-                    if(row.Count>12) Console.WriteLine($"{row[0] + $" не внесли деньги за {monthName} - " + row[12].ToString()}");
+                if ((long.TryParse(row[nowMountRow - 1].ToString(), out long resultBackMounth) && resultBackMounth != 0) && (long.TryParse(row[nowMountRow].ToString(), out long resultCurMounth) && resultCurMounth == 0) && (long.TryParse(row[nowMountRow + 1].ToString(), out long resultNextMounth) && resultNextMounth == 0))
+                {
+                    if (row.Count > 12) Console.WriteLine($"{row[0] + $" не внесли деньги за {monthName} - " + row[12].ToString()}");
                 }
             }
-		}
-		else
-		{
-			Console.WriteLine("No data found.");
-		}
-	}
+        }
+        else
+        {
+            Console.WriteLine("No data found.");
+        }
+    }
 
     static async Task DeleteNotification(string notificationId)
     {
@@ -292,84 +328,90 @@ class Program
         }
     }
 }
+
     public class Root
-{
-    [JsonProperty("receiptId")]
-    public int ReceiptId { get; set; }
+    {
+        [JsonProperty("receiptId")]
+        public int ReceiptId { get; set; }
 
-    [JsonProperty("body")]
-    public Body Body { get; set; }
-}
+        [JsonProperty("body")]
+        public Body Body { get; set; }
+    }
+    public class Auth
+    {
+        [JsonProperty("stateInstance")]
+        public string stateInstance { get; set; }
+    }
 
-public class Body
-{
-    [JsonProperty("from")]
-    public string From { get; set; }
-    
-    [JsonProperty("typeWebhook")]
-    public string TypeWebhook { get; set; }
+    public class Body
+    {
+        [JsonProperty("from")]
+        public string From { get; set; }
 
-    [JsonProperty("instanceData")]
-    public InstanceData InstanceData { get; set; }
+        [JsonProperty("typeWebhook")]
+        public string TypeWebhook { get; set; }
 
-    [JsonProperty("timestamp")]
-    public long Timestamp { get; set; }
+        [JsonProperty("instanceData")]
+        public InstanceData InstanceData { get; set; }
 
-    [JsonProperty("status")]
-    public string Status { get; set; }
+        [JsonProperty("timestamp")]
+        public long Timestamp { get; set; }
 
-    [JsonProperty("idMessage")]
-    public string IdMessage { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
 
-    [JsonProperty("senderData")]
-    public SenderData SenderData { get; set; }
+        [JsonProperty("idMessage")]
+        public string IdMessage { get; set; }
 
-    [JsonProperty("messageData")]
-    public MessageData MessageData { get; set; }
-}
+        [JsonProperty("senderData")]
+        public SenderData SenderData { get; set; }
 
-public class InstanceData
-{
-    [JsonProperty("idInstance")]
-    public long IdInstance { get; set; }
+        [JsonProperty("messageData")]
+        public MessageData MessageData { get; set; }
+    }
 
-    [JsonProperty("wid")]
-    public string Wid { get; set; }
+    public class InstanceData
+    {
+        [JsonProperty("idInstance")]
+        public long IdInstance { get; set; }
 
-    [JsonProperty("typeInstance")]
-    public string TypeInstance { get; set; }
-}
+        [JsonProperty("wid")]
+        public string Wid { get; set; }
+
+        [JsonProperty("typeInstance")]
+        public string TypeInstance { get; set; }
+    }
     class Debtor
     {
         public string ChatId { get; set; }
         public string Message { get; set; }
     }
     public class SenderData
-{
-    [JsonProperty("chatId")]
-    public string ChatId { get; set; }
+    {
+        [JsonProperty("chatId")]
+        public string ChatId { get; set; }
 
-    [JsonProperty("chatName")]
-    public string ChatName { get; set; }
+        [JsonProperty("chatName")]
+        public string ChatName { get; set; }
 
-    [JsonProperty("sender")]
-    public string Sender { get; set; }
+        [JsonProperty("sender")]
+        public string Sender { get; set; }
 
-    [JsonProperty("senderName")]
-    public string SenderName { get; set; }
-}
+        [JsonProperty("senderName")]
+        public string SenderName { get; set; }
+    }
 
-public class MessageData
-{
-    [JsonProperty("typeMessage")]
-    public string TypeMessage { get; set; }
+    public class MessageData
+    {
+        [JsonProperty("typeMessage")]
+        public string TypeMessage { get; set; }
 
-    [JsonProperty("textMessageData")]
-    public TextMessageData TextMessageData { get; set; }
-}
+        [JsonProperty("textMessageData")]
+        public TextMessageData TextMessageData { get; set; }
+    }
 
-public class TextMessageData
-{
-    [JsonProperty("textMessage")]
-    public string TextMessage { get; set; }
-}
+    public class TextMessageData
+    {
+        [JsonProperty("textMessage")]
+        public string TextMessage { get; set; }
+    }
